eca319ad1dc6032ebaf21872883dd36b9f2c529c
Punit Gupta <Punit Gupta@e7491386-0724-0410-be41-cfab96959941>
2019-01-30 15:19:26 +0000

    POD Performance Tuning
    git-svn-id: https://svn.na.odcorp.net/svn/od/crm/trunk/xxcrm@293093 e7491386-0724-0410-be41-cfab96959941

---
+++ admin/sql/XX_POD_DROP_INDEX_XXCRM.sql
@@ -0,0 +1,27 @@
+-- +============================================================================================+
+-- |                        Office Depot - Project Simplify                                     |
+-- |                                                                                            |
+-- +============================================================================================+
+-- | Name         : XX_POD_DROP_INDEX.sql                                            	|
+-- | Rice Id      :                                                                             | 
+-- | Description  : Drop all indexes                     					|	  
+-- | Purpose      :                                                                             |
+-- |                                                                                            |
+-- |                                                                                            |
+-- |Change Record:                                                                              |
+-- |===============                                                                             |
+-- |Version    Date          Author                Remarks                                      | 
+-- |=======    ==========    =================    ==============================================+
+-- |1.0        30-JAN-2019   CG       Initial Version                               |
+-- +============================================================================================+
+
+SET SHOW         OFF
+SET VERIFY       OFF
+SET ECHO         OFF
+SET TAB          OFF
+SET FEEDBACK     ON
+
+
+drop index XXCRM.XX_CDH_CUST_ACCT_EXT_B_N7;
+
+
