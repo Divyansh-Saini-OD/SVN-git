1c8aba724d6c90d3277b2bf327960bb39aa522e1
Priyam Parmar <Priyam Parmar@e7491386-0724-0410-be41-cfab96959941>
2019-02-14 08:34:37 +0000

    DB link removed for LNS1
    git-svn-id: https://svn.na.odcorp.net/svn/od/finance/trunk/xxfin@293525 e7491386-0724-0410-be41-cfab96959941

--- sql/XXRETTDMXTRCT.sql
+++ sql/XXRETTDMXTRCT.sql
@@ -10,6 +10,7 @@ REM     OUTPUTS                 :
 REM     HISTORY                 :  WHO -        WHAT -          DATE -
 REM     NOTES                   :  RAJI NATARAJAN, Wipro , Fixed defect 6452
 REM     NOTES                   :  Sandeep Pandhare, Defect 10891 - Remove timestamp from filename
+REM     NOTES                   :  Naveen Srinivas Db link removed LNS1
 REM    ______________________________________________________________________________
 
 set concat .
@@ -45,72 +46,62 @@ SELECT directory_path
 FROM   dba_directories
 WHERE  directory_name = 'XXFIN_OUTBOUND';
 
-SELECT '|'
-       ||to_char(COUNT(* )) row_count
-FROM   hr_locations_all h,
-       od.loc@legacydb2 l,
-       od.region@legacydb2 r,
-       od.distrct@legacydb2 d
-WHERE  ltrim(substr(h.location_code,1,6),'0') = to_char(l.loc_id (+) )
-       AND l.region_id = r.region_id
-       AND l.district_id = d.district_id
-       AND h.country IN ('US',
-                         'CA');
+SELECT '|' 
+       || TO_CHAR(COUNT(*)) row_count 
+  FROM hr_locations_all h 
+ WHERE h.country IN ( 'US', 'CA' ) 
+   AND h.location_code LIKE '00%'
+   AND TRUNC(NVL(h.inactive_date, sysdate)) >= TRUNC(sysdate); 
                          
 REM Defect 10891                         
 spool &p_outdir.TDM_RETXTRCT.dat
 
 PROMPT LOCATION CODE|DESCR|PHONE NBR|FAX NBR|MGR FNAME|MGR LNAME|ADDR LINE 1|ADDR LINE 2|CITY|STATE|ZIP|REGION ID|REGION DESCR|REGIONAL MGR FNAME|REGIONAL MGR LNAME|DISTRICT ID|DISTRICT DESCR|DISTRICT MGR FNAME|DISTRICT MGR LNAME|COUNTRY CODE
 
-SELECT   substr(h.location_code,1,6)
-         ||'|'
-         ||substr(l.descr,1,25)
-         ||'|'
-         ||l.phone_nbr
-         ||'|'
-         ||l.fax_nbr
-         ||'|'
-         ||substr(l.mgr_fname,1,10)
-         ||'|'
-         ||substr(l.mgr_lname,1,15)
-         ||'|'
-         ||substr(l.addr_line_1,1,30)
-         ||'|'
-         ||substr(l.addr_line_2,1,30)
-         ||'|'
-         ||substr(l.city,1,28)
-         ||'|'
-         ||substr(l.state,1,2)
-         ||'|'
-         ||substr(l.zip,1,9)
-         ||'|'
-         ||l.region_id
-         ||'|'
-         ||substr(r.descr,1,30)
-         ||'|'
-         ||substr(r.mgr_fname,1,10)
-         ||'|'
-         ||substr(r.mgr_lname,1,15)
-         ||'|'
-         ||l.district_id
-         ||'|'
-         ||substr(d.descr,1,30)
-         ||'|'
-         ||substr(d.mgr_fname,1,10)
-         ||'|'
-         ||substr(d.mgr_lname,1,15)
-         ||'|'
-         ||l.country_cd
-FROM     hr_locations_all h,
-         od.loc@legacydb2 l,
-         od.region@legacydb2 r,
-         od.distrct@legacydb2 d
-WHERE    ltrim(substr(h.location_code,1,6),'0') = to_char(l.loc_id (+) )
-         AND l.region_id = r.region_id
-         AND l.district_id = d.district_id
-         AND h.country IN ('US',
-                           'CA')
-ORDER BY 1;
+SELECT SUBSTR (h.location_code, 1, 6) 
+       || '|' 
+       || SUBSTR (h.description, INSTR(h.description, ':') + 1, 25) 
+       || '|' 
+       || REGEXP_REPLACE(h.telephone_number_1, '[^0-9]', '') 
+       || '|' 
+       || REGEXP_REPLACE(h.telephone_number_2, '[^0-9]', '') 
+       || '|' 
+       || SUBSTR (h.loc_information15, 1, INSTR(h.loc_information15, ' ') - 1) 
+       || '|' 
+       || SUBSTR (h.loc_information15, INSTR(h.loc_information15, ' ') + 1, 15) 
+       || '|' 
+       || SUBSTR (h.address_line_1, 1, 30) 
+       || '|' 
+       || SUBSTR (h.address_line_2, 1, 30) 
+       || '|' 
+       || SUBSTR (h.town_or_city, 1, 28) 
+       || '|' 
+       || SUBSTR (h.region_2, 1, 2) 
+       || '|' 
+       || SUBSTR (h.postal_code, 1, 9) 
+       || '|' 
+       || NULL 
+       || '|' 
+       || NULL 
+       || '|' 
+       || NULL 
+       || '|' 
+       || NULL 
+       || '|' 
+       || NULL 
+       || '|' 
+       || NULL 
+       || '|' 
+       || NULL 
+       || '|' 
+       || NULL 
+       || '|' 
+       || DECODE (h.country, 'US', 'USA', 'CA', 'CAN', h.country)
+FROM   hr_locations_all h 
+WHERE  h.country IN ( 'US', 'CA' ) 
+  AND  h.location_code LIKE '00%'
+  AND  TRUNC (NVL (h.inactive_date, sysdate)) >= TRUNC (sysdate) 
+ORDER  BY 1;
 prompt 3|TDM_RETXTRCT&p_filedate.dat&p_traildate.&p_rowcount
 spool off
 
