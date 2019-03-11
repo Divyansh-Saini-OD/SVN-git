c8f58e85c0f8c50ec430b3a9510714e4b52d6424
Dinesh Nagapuri <Dinesh Nagapuri@e7491386-0724-0410-be41-cfab96959941>
2019-03-11 17:07:54 +0000

    Lift and Shift.
    git-svn-id: https://svn.na.odcorp.net/svn/od/finance/trunk/xxfin/bin@294326 e7491386-0724-0410-be41-cfab96959941

---
+++ XXPROCESSLFTP.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+	 echo "Executing LFTP in Process LFTP File"
+	 echo "user $1"
+	 echo "passwd $2"
+	 echo "port $3"
+	 echo "server $4"
+	 echo "source Location $5"
+	 echo "Destination Location  $6"
+	 echo "Source File $7"
+	 echo "Temp File $8"
+	 echo "-u $user,$passwd -p $server"
+	 echo "-u $1,$2 -p $3 $4"
+	 LFTPPARMS2="-u $1,$2 -p $3 $4"
+	 echo "LFTPPARMS2 $LFTPPARMS2"
+	 echo "Before lftp"
+success=`/usr/bin/lftp -u $1,$2 -p $3 $4 <<EOF
+lcd $5
+cd  $6
+put $7
+ls $7 | wc -l
+quit
+EOF
+`
+echo success=$success
+if [ $success = 1 ]
+then
+	echo " LFTP transfer completed successfully " > $8 
+fi
+
