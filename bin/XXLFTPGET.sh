a3bd9a6012798f568409b74554a7f9de5e94442c
Havish Kasina <Havish Kasina@e7491386-0724-0410-be41-cfab96959941>
2019-03-27 19:46:29 +0000

    Modified for Lift and Shift
    git-svn-id: https://svn.na.odcorp.net/svn/od/finance/trunk/xxfin@294851 e7491386-0724-0410-be41-cfab96959941

---
+++ bin/XXLFTPGET.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+echo "Executing LFTP in XXLFTPGET File"
+echo "user $1"
+echo "passwd $2"
+echo "port $3"
+echo "server $4"
+echo "source Location $5"
+echo "Destination Location  $6"
+echo "Source File $7"
+echo "Destination File $8"
+echo "-u $user,$passwd -p $server"
+echo "-u $1,$2 -p $3 $4"
+LFTPPARMS2="-u $1,$2 -p $3 $4"
+echo "LFTPPARMS2 $LFTPPARMS2"
+echo "Before lftp"
+/usr/bin/lftp -u $1,$2 -p $3 $4 <<EOF
+cd $5
+lcd  $6
+mget $7
+quit
+EOF
+echo "After lftp"
+#The following commands will be executed in local machine
+success=`ls $6/$7 | wc -l`
+echo success=$success
+if [ $success -ge 1 ]
+then
+    #If the destination file name is not null then move the file to destination file
+	if [ "$8" != "" ]
+    then	
+	   mv $6/$7 $6/$8
+	fi
+	echo "LFTP_SUCCESSFUL"
+fi
+echo "Executiing XXLFTPGET is completed"
+
